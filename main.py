9821c60946392ba81257391390e99701acc9b284
Chew Hoa Shen <chewhoashen@gmail.com>
2021-07-18 18:02:49 +0800

    Add service.py

--- main.py
+++ main.py
@@ -1,29 +1,44 @@
+from service import getSlots
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 import logging
+import json
 
-updater = Updater(token='1929838326:AAF4GHWsAQyl_U1GQDsiL6GGSDSGxNGJ2dw', use_context=True)
-dispatcher = updater.dispatcher
+def getData():
+    file = open('data.json')
+    data = json.load(file)
+    return data
 
-logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
 
-def start(update, context):
-    textmsg = f'Hello {update.effective_user.first_name}, I am NUS Gym Bot. \nUse "/slots" to view available gym slots in NUS.'
-    context.bot.send_message(chat_id=update.effective_chat.id, text=textmsg)
+def main():
+    data = getData()
 
-def slots(update, context):
-    context.bot.send_message(chat_id=update.effective_chat.id, text="Available slots:")
-    
-def caps(update, context):
-    text_caps = ' '.join(context.args).upper()
-    context.bot.send_message(chat_id=update.effective_chat.id, text=text_caps)
+    updater = Updater(token=data['token'], use_context=True)
+    dispatcher = updater.dispatcher
 
-def unknown(update, context):
-    context.bot.send_message(chat_id=update.effective_chat.id, text="Sorry, that is not a valid command.")
+    logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
 
-updater.dispatcher.add_handler(CommandHandler('start', start))
-updater.dispatcher.add_handler(CommandHandler('slots', slots))
-updater.dispatcher.add_handler(CommandHandler('caps', caps))
-updater.dispatcher.add_handler(MessageHandler(Filters.command, unknown))
+    def start(update, context):
+        textmsg = f'Hello {update.effective_user.first_name}, I am NUS Gym Bot. \nUse "/slots" to view available gym slots in NUS.'
+        context.bot.send_message(chat_id=update.effective_chat.id, text=textmsg)
 
-updater.start_polling()
-updater.idle()
+    def slots(update, context):
+        title = getSlots(data["driver_path"])
+        context.bot.send_message(chat_id=update.effective_chat.id, text=f'Available slots: {title}')
+        
+    def caps(update, context):
+        text_caps = ' '.join(context.args).upper()
+        context.bot.send_message(chat_id=update.effective_chat.id, text=text_caps)
+
+    def unknown(update, context):
+        context.bot.send_message(chat_id=update.effective_chat.id, text="Sorry, that is not a valid command.")
+
+    updater.dispatcher.add_handler(CommandHandler('start', start))
+    updater.dispatcher.add_handler(CommandHandler('slots', slots))
+    updater.dispatcher.add_handler(CommandHandler('caps', caps))
+    updater.dispatcher.add_handler(MessageHandler(Filters.command, unknown))
+
+    updater.start_polling()
+    updater.idle()
+
+if __name__ == "__main__":
+    main()
--- service.py
+++ service.py
@@ -1,1 +1,13 @@
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
+def getSlots(driver_path):
+    options = Options()
+    options.headless = True
+    options.add_argument("--window-size=1920,1200")
+
+    driver = webdriver.Chrome(executable_path=driver_path, options=options)
+    driver.get("https://reboks.nus.edu.sg/nus_public_web/public/profile/buypass/gym")
+
+    return driver.title
+
