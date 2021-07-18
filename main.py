cfcf7f22ec3c352e8daca2838412c0d6e45240b8
Chew Hoa Shen <chewhoashen@gmail.com>
2021-07-18 16:30:18 +0800

    Add basic commands

--- main.py
+++ main.py
@@ -1,14 +1,30 @@
 from telegram import Update
-from telegram.ext import Updater, CommandHandler, CallbackContext
+from telegram.ext import Updater, CommandHandler, CallbackContext, MessageHandler, Filters
+import logging
 
+updater = Updater(token='1929838326:AAF4GHWsAQyl_U1GQDsiL6GGSDSGxNGJ2dw', use_context=True)
+dispatcher = updater.dispatcher
 
-def hello(update: Update, context: CallbackContext) -> None:
-    update.message.reply_text(f'Hello {update.effective_user.first_name}')
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
 
+def start(update, context):
+    textmsg = f'Hello {update.effective_user.first_name}, I am NUS Gym Bot. \nUse "/slots" to view available gym slots in NUS.'
+    context.bot.send_message(chat_id=update.effective_chat.id, text=textmsg)
 
-updater = Updater('1929838326:AAF4GHWsAQyl_U1GQDsiL6GGSDSGxNGJ2dw')
+def slots(update, context):
+    context.bot.send_message(chat_id=update.effective_chat.id, text="Available slots:")
+    
+def caps(update, context):
+    text_caps = ' '.join(context.args).upper()
+    context.bot.send_message(chat_id=update.effective_chat.id, text=text_caps)
 
-updater.dispatcher.add_handler(CommandHandler('hello', hello))
+def unknown(update, context):
+    context.bot.send_message(chat_id=update.effective_chat.id, text="Sorry, that is not a valid command.")
+
+updater.dispatcher.add_handler(CommandHandler('start', start))
+updater.dispatcher.add_handler(CommandHandler('slots', slots))
+updater.dispatcher.add_handler(CommandHandler('caps', caps))
+updater.dispatcher.add_handler(MessageHandler(Filters.command, unknown))
 
 updater.start_polling()
 updater.idle()
